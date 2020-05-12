73c09282e90811a8dc721e8b14031313fbf4b0d0
Alexander George <alexander97.george@gmail.com>
2020-05-12 14:40:20 -0700

    style changes; advanced filter button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './NavBar';
 import { useLocation } from 'react-router-dom';
 
@@ -29,44 +29,42 @@ function SearchResults() {
 
 function SearchFilter() {
 
-    // const keyword, radius, cuisines, categories
+    const [showAdvanced, setShowAdvanced] = useState(false);
+
 
     return (
         <section className='Search-Filter'>
 
-            {/* <section style={{justifyContent: 'center'}}>
-                <span className='SF-price'>
-                    <p>$</p>
-                    <p>$$</p>
-                    <p>$$$</p>
-                    <p>$$$$</p>
-                </span>
-            </section> */}
-            
+
             <SearchFilterSection 
                 title={'Category'} optionsArray={['Delivery', 'Dine-out', 
                 'Nightlife', 'Cafes', 'Breakfast', 'Lunch', 'Dinner', 'Bars', 
                 'Clubs']} type={'checkbox'}
             />
-            <SearchFilterSection
-                title={'Cuisine'} optionsArray={['American', 'Asian', 'BBQ', 'Bakery',
-                'Bar Food', 'Beverages', 'Breakfast', 'Burgers', 'Cafe', 'Cajun', 'Chinese',
-                'Coffee', 'Crepes', 'Deli', 'Desserts', 'Diner', 'Donuts', 'Fast Food',
-                'Filipino', 'French', 'German', 'Greek', 'Healthy', 'Ice Cream', 'Indian',
-                'International', 'Italian', 'Japanese', 'Juices', 'Korean', 'Latin', 'Mediterranean',
-                'Mexican', 'Middle Eastern', 'Mongolian', 'New American', 'Pizza', 'Russian', 
-                'Salad', 'Sandwich', 'Seafood', 'Southern', 'Steak', 'Sushi', 'Taco', 'Tapas',
-                'Tea', 'Teriyaki', 'Tex-Mex', 'Thai', 'Vegetarian', 'Vietnamese']} type={'checkbox'}
-            />
-            <SearchFilterSection 
-                title={'Establishment'} optionsArray={['Quick Bites', 'Sandwich Shop', 'Cafe',
-                'Fast Food', 'Bar', 'Casual Dining', 'Deli', 'Bakery', 'Fine Dining', 'Pizzeria',
-                'Diner', 'Lounge', 'Wine Bar', 'Pub', 'Coffee Shop', 'Dessert Parlour', 'Club', 
-                'Bistro', 'Fast Casual', 'Brewery', 'Juice Bar', 'Food Court', 'Taqueria', 'Noddle Shop',
-                'Beer Garden', 'Steakhouse', 'Cocktail Bar', 'Beverage Shop', 'Sweet Shop', 'Food Truck',
-                'Izakaya', 'Microbrewery', 'Vineyard', 'Shack']} type={'checkbox'}
-            />
-
+            <button onClick={() => setShowAdvanced(!showAdvanced)} className='SF-advanced'>
+                {showAdvanced ? 'hide advanced filters' : 'show advanced filters'}
+            </button>
+            {showAdvanced ? <section style={{ width: '100%' }}>
+                <SearchFilterSection
+                    title={'Cuisine'} optionsArray={['American', 'Asian', 'BBQ', 'Bakery',
+                    'Bar Food', 'Beverages', 'Breakfast', 'Burgers', 'Cafe', 'Cajun', 'Chinese',
+                    'Coffee', 'Crepes', 'Deli', 'Desserts', 'Diner', 'Donuts', 'Fast Food',
+                    'Filipino', 'French', 'German', 'Greek', 'Healthy', 'Ice Cream', 'Indian',
+                    'International', 'Italian', 'Japanese', 'Juices', 'Korean', 'Latin', 'Mediterranean',
+                    'Mexican', 'Middle Eastern', 'Mongolian', 'New American', 'Pizza', 'Russian', 
+                    'Salad', 'Sandwich', 'Seafood', 'Southern', 'Steak', 'Sushi', 'Taco', 'Tapas',
+                    'Tea', 'Teriyaki', 'Tex-Mex', 'Thai', 'Vegetarian', 'Vietnamese']} type={'checkbox'}
+                />
+                <SearchFilterSection 
+                    title={'Establishment'} optionsArray={['Quick Bites', 'Sandwich Shop', 'Cafe',
+                    'Fast Food', 'Bar', 'Casual Dining', 'Deli', 'Bakery', 'Fine Dining', 'Pizzeria',
+                    'Diner', 'Lounge', 'Wine Bar', 'Pub', 'Coffee Shop', 'Dessert Parlour', 'Club', 
+                    'Bistro', 'Fast Casual', 'Brewery', 'Juice Bar', 'Food Court', 'Taqueria', 'Noddle Shop',
+                    'Beer Garden', 'Steakhouse', 'Cocktail Bar', 'Beverage Shop', 'Sweet Shop', 'Food Truck',
+                    'Izakaya', 'Microbrewery', 'Vineyard', 'Shack']} type={'checkbox'}
+                />
+            </section> : null }
+            <button className='SF-submit'>Apply Filters</button>
         </section>
     )
 }

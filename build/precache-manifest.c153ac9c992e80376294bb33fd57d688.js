60f7ea987ae1a58e3b6b7f534cc356f6e908501e
Alexander George <alexander97.george@gmail.com>
2020-05-22 12:22:13 -0700

    more firebase stuff

---
+++ build/precache-manifest.c153ac9c992e80376294bb33fd57d688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159636ca9686b28ab2719ff610fc930f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0385e6f476ccfd58738",
+    "url": "/static/css/main.ca77ee03.chunk.css"
+  },
+  {
+    "revision": "da5bf99fe8f596a3cdd3",
+    "url": "/static/js/2.ca843e76.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.ca843e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0385e6f476ccfd58738",
+    "url": "/static/js/main.8a452b19.chunk.js"
+  },
+  {
+    "revision": "0ac5d895fcdff14a9596",
+    "url": "/static/js/runtime-main.c1c1638a.js"
+  },
+  {
+    "revision": "d4ae132e92f77db43fb61630753ef0f2",
+    "url": "/static/media/dinner.d4ae132e.jpg"
+  }
+]);

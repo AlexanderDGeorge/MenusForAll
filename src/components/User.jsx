bb41dcf295ffd9f928b9edb86240d51a8efae0a2
Alexander George <alexander97.george@gmail.com>
2020-05-26 11:43:53 -0700

    search bar update on location; default profile image

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,27 +2,33 @@ import React, { useContext, useState } from 'react';
 import { UserContext } from './Application';
 import { signOut } from '../firebase';
 import { MdKeyboardArrowDown } from 'react-icons/md';
+import defaultProfileImg from '../assets/defaultProfileImg.jpg';
 
 export default function User() {
-    const { user } = useContext(UserContext);
-    const [open, setOpen] = useState(false);
+	const { user } = useContext(UserContext);
+	const [open, setOpen] = useState(false);
 
-    return (
-        <div className='User' onClick={() => setOpen(!open)}>
-            <img src={user['photoURL']} alt=""/>
-            <MdKeyboardArrowDown style={{
-                height: 24,
-                width: 'auto'
-            }}/>
-            {open ? <UserDropdown /> : null}
-        </div>
-    )
+	return (
+		<div className="User" onClick={() => setOpen(!open)}>
+			<img
+				src={user['photoURL'] ? user['photoURL'] : defaultProfileImg}
+				alt=""
+			/>
+			<MdKeyboardArrowDown
+				style={{
+					height: 24,
+					width: 'auto',
+				}}
+			/>
+			{open ? <UserDropdown /> : null}
+		</div>
+	);
 }
 
 function UserDropdown() {
-    return (
-        <div className='UserDropdown'>
-            <button onClick={signOut}>Log Out</button>
-        </div>
-    )
+	return (
+		<div className="UserDropdown">
+			<button onClick={signOut}>Log Out</button>
+		</div>
+	);
 }
